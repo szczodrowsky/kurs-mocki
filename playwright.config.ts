026a5d33b2bc458b9bdd3da092a0615b707748d5
szczodrowsky <adam.szczodro@gmail.com>
2026-08-26 20:56:58 +0200

    Add TS to project

---
+++ playwright.config.ts
@@ -0,0 +1,19 @@
+import { defineConfig, devices } from '@playwright/test';
+
+export default defineConfig({
+  testDir: './src/tests',
+  outputDir: './tests-results',
+  fullyParallel: true,
+  retries:  0,
+  workers: 1,
+  reporter: 'html',
+  use: {
+    trace: 'on-first-retry',
+  },
+  projects: [
+    {
+      name: 'mocking_tests',
+      use: { ...devices['Desktop Chrome'] },
+    },
+  ],
+});
